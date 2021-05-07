b7990c5ccb376a73e9303dd966d67a1085eb5b00
Gauthier3090 <gauthier.pladet@gmail.com>
2021-05-07 23:21:43 +0200

    free leaks

--- srcs_swap/main.c
+++ srcs_swap/main.c
@@ -6,20 +6,37 @@
 /*   By: gpladet <gpladet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 17:51:59 by gpladet           #+#    #+#             */
-/*   Updated: 2021/05/07 22:12:20 by gpladet          ###   ########.fr       */
+/*   Updated: 2021/05/07 23:08:43 by gpladet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes_swap/header.h"
 
+void	free_tab(char **tab)
+{
+	int	i;
+
+	if (!tab)
+		return ;
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+}
+
 char	**split_arg_env(char **argv)
 {
 	char	*env;
+	char	*tmp;
 
-	argv[0] = ft_strjoin(argv[0], " ");
-	if (!argv[0])
+	tmp = ft_strjoin(argv[0], " ");
+	if (!tmp)
 		exit(EXIT_FAILURE);
-	env = ft_strjoin(argv[0], argv[1]);
+	env = ft_strjoin(tmp, argv[1]);
+	free(tmp);
 	if (!env)
 		exit(EXIT_FAILURE);
 	argv = ft_split(env, ' ');
@@ -34,11 +51,16 @@ int	main(int argc, char **argv)
 	t_double_linked_list		*list;
 	t_double_linked_list_node	*node;
 	int							i;
+	int							is_malloc;
 
 	if (argc > 1)
 	{
+		is_malloc = FALSE;
 		if (argc == 2)
+		{
 			argv = split_arg_env(argv);
+			is_malloc = TRUE;
+		}
 		list = new_list();
 		i = 0;
 		while (argv[++i])
@@ -46,9 +68,9 @@ int	main(int argc, char **argv)
 			node = new_node(argv[i]);
 			insert(list, node);
 		}
-		display_list_prev(list);
-		ft_putchar_fd('\n', 1);
 		display_list_next(list);
 		free_list(list);
+		if (is_malloc)
+			free_tab(argv);
 	}
 }
