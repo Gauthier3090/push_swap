0f92e7d9d395a898d9a181ea1c0c0d659982aef3
Gauthier_3090 <gauthier.pladet@gmail.com>
2021-05-07 11:52:33 +0200

    fix segfault

--- srcs_swap/main.c
+++ srcs_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: gpladet <gpladet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 17:51:59 by gpladet           #+#    #+#             */
-/*   Updated: 2021/05/06 18:23:43 by gpladet          ###   ########.fr       */
+/*   Updated: 2021/05/07 11:48:16 by gpladet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,16 @@ void	insert(t_double_linked_list *list, t_double_linked_list_node *new_node)
 	int	i;
 
 	if (list->count == 0)
+	{
 		list->current = new_node;
+		list->current->prev = new_node;
+		list->current->next = new_node;
+	}
 	else if (list->count == 1)
 	{
 		new_node->prev = list->current;
 		new_node->next = list->current;
 		list->current->prev = new_node;
-		list->current->next = new_node;
 		list->current = new_node;
 	}
 	else
@@ -58,7 +61,7 @@ void	insert(t_double_linked_list *list, t_double_linked_list_node *new_node)
 		new_node->prev = list->current;
 		list->current->next = new_node;
 		i = -1;
-		while (++i < list->count -1)
+		while (++i < list->count - 1)
 			list->current = list->current->prev;
 		new_node->next = list->current;
 		list->current->prev = new_node;
@@ -71,6 +74,8 @@ void	swap(t_double_linked_list *list)
 {
 	int	value;
 
+	if (list->count == 1)
+		return ;
 	value = list->current->value;
 	list->current->value = list->current->prev->value;
 	list->current->prev->value = value;
@@ -100,7 +105,7 @@ void	free_list(t_double_linked_list *list)
 	free(list);
 }
 
-void 	display_list(t_double_linked_list *list)
+void	display_list(t_double_linked_list *list)
 {
 	int	i;
 
@@ -129,15 +134,6 @@ int	main(int argc, char **argv)
 			node = new_node(argv[i]);
 			insert(list, node);
 		}
-		display_list(list);
-		swap(list);
-		ft_putchar_fd('\n', 1);
-		display_list(list);
-		ft_putchar_fd('\n', 1);
-		rotate(list);
-		display_list(list);
-		ft_putchar_fd('\n', 1);
-		reverse_rotate(list);
 		display_list(list);
 		free_list(list);
 	}
